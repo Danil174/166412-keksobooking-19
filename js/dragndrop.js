9bd491c316e31db951d7e37d631acd7ec0719494
Danil <milostiviy.danil@yandex.ru>
2020-03-07 23:44:51 +0500

    рефакторинг функции itemDragNDrop: добавил функцию проверки выхода за границы

--- js/dragndrop.js
+++ js/dragndrop.js
@@ -1,6 +1,18 @@
 'use strict';
 
 (function () {
+  var checkNumberInInterval = function (number, lowerLimit, upperLimit) {
+    if (number > lowerLimit && number < upperLimit) {
+      return number;
+    } else {
+      if (number < lowerLimit) {
+        return lowerLimit;
+      } else {
+        return upperLimit;
+      }
+    }
+  };
+
   var itemDragNDrop = function (item, area) {
     item.addEventListener('mousedown', function (evt) {
       evt.preventDefault();
@@ -19,21 +31,15 @@
       var onMouseMove = function (moveEvt) {
         moveEvt.preventDefault();
 
-        if (moveEvt.clientX - startCoords.x < 0) {
-          shift.x = 0;
-        } else if (moveEvt.clientX - startCoords.x + item.offsetWidth > area.clientWidth) {
-          shift.x = area.clientWidth - item.offsetWidth;
-        } else {
-          shift.x = moveEvt.clientX - startCoords.x;
-        }
+        var itemPositionX = moveEvt.clientX - startCoords.x;
+        var itemPositionY = moveEvt.clientY - startCoords.y;
+        var leftBorder = -item.offsetWidth / 2;
+        var rightBorder = area.clientWidth - item.offsetWidth / 2;
+        var topBorder = window.constants.userPinParams.MIN_Y - window.constants.userPinParams.HEIGHT;
+        var bottomBorder = window.constants.userPinParams.MAX_Y - window.constants.userPinParams.HEIGHT;
 
-        if (moveEvt.clientY - startCoords.y < window.constants.userPinParams.MIN_Y - window.constants.userPinParams.HEIGHT) {
-          shift.y = window.constants.userPinParams.MIN_Y - window.constants.userPinParams.HEIGHT;
-        } else if (moveEvt.clientY - startCoords.y + window.constants.userPinParams.HEIGHT > window.constants.userPinParams.MAX_Y) {
-          shift.y = window.constants.userPinParams.MAX_Y - window.constants.userPinParams.HEIGHT;
-        } else {
-          shift.y = moveEvt.clientY - startCoords.y;
-        }
+        shift.x = checkNumberInInterval(itemPositionX, leftBorder, rightBorder);
+        shift.y = checkNumberInInterval(itemPositionY, topBorder, bottomBorder);
 
         item.style.top = shift.y + 'px';
         item.style.left = shift.x + 'px';
