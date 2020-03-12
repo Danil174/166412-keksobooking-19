efc8f058ba31a115274bde6f239969d627a66890
Danil <milostiviy.danil@yandex.ru>
2020-03-12 15:06:38 +0500

    show avatar WIP

---
+++ js/avatar.js
@@ -0,0 +1,27 @@
+'use strict';
+
+(function () {
+  var avatarInput = document.querySelector('#avatar');
+  var preview = document.querySelector('.ad-form-header__preview img');
+
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+  avatarInput.addEventListener('change', function () {
+    var file = avatarInput.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var mathces = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (file) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        preview.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+})();
